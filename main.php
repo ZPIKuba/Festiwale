9466dc6e0c135aff5f8695d7fbcbd0b0e18a041b
wiejskichlop <wiejskichlop@gmail.com>
2017-04-24 20:21:12 +0200

    Update menu, przeniesienie menu do osobnego pliku

--- main.php
+++ main.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Festiwale</title>
-    <link href="menu.css" rel="stylesheet" >
 
 </head>
 <body>
 
 <?php
-readfile(menu.html);
+readfile('menu.html');
 ?>
+
 
 </body>
 </html>
