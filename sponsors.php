660275c9a15addf339d5db5bcd04c40229292b46
Zenek <zenekurwinoga@gmail.com>
2017-05-29 00:00:15 +0200

    Pliki Ewy

--- sponsors.php
+++ sponsors.php
@@ -3,13 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <title>Sponsorzy</title>
+    <link href="menu.css" rel="stylesheet" >
 </head>
 <body>
 
 <?php
 readfile('menu.html');
 ?>
-
-<img src="nokia.jpeg" alt="sponsor" width="420" height="240" align="middle" />
+<table align=center style="width:70%">
+    <tr>
+        <th></th>
+        <th></th>
+        <th><img src="\res\862px-Herb_wroclaw.svg.png" alt="Prezydent Wrocławia" width="100" height="100" align="middle" /></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <tr>
+        <th><img src="\res\logotyp_cirs1004x546.png" alt="CIRS" width="100" height="100" align="middle" /></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th><img src="\res\metro.png" alt="Wrocław" width="100" height="100" align="middle" /></th>
+    </tr>
+    <tr>
+        <th></th>
+        <th><img src="\res\Rada Osiedla Ołbin.png" alt="Ołbin" width="100" height="100" align="middle" /></th>
+        <th></th>
+        <th><img src="\res\lili.png" alt="Światowid" width="100" height="100" align="middle" /></th>
+        <th></th>
+    </tr>
+    <tr>
+        <th></th>
+        <th></th>
+        <th><img src="\res\mpk.png" alt="Barwy" width="100" height="100" align="middle" /></th>
+        <th></th>
+        <th></th>
+    </tr>
+</table>
 </body>
 </html>
