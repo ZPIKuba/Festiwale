8fde9e91328fbf25be29836617193b835941b807
Zenek <zenekurwinoga@gmail.com>
2017-05-29 01:03:58 +0200

    Galeria + weź udział

---
+++ galeria.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang=pl>
+<head>
+    <meta charset="UTF-8">
+    <title>Galeria</title>
+</head>
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zenek
+ * Date: 29.05.2017
+ * Time: 00:42
+ */
+
+readfile('menu.html');
+?>
+
+
+<div style="margin-left: 5%;width: 80%">
+    <h2 style="font-size: 25px;margin-top: 10%">
+        Festiwal Chórów Osiedlowych - galeria 2015 by Stanisław Stanisławowski
+    </h2>
+
+    <ul style="list-style-type: none">
+        <li style="float: left;margin-left: 3%""><img src="res/galeria/choirOne.jpg" style="height: 200px"></li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirTwo.jpg" style="height: 200px" </li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirThree.jpg" style="height: 200px"</li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirNine.jpg" style="height: 200px"</li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirFour.jpg" style="height: 200px"</li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirFive.jpg" style="height: 200px"</li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirSix.jpg" style="height: 200px"</li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirSeven.jpg" style="height: 200px"</li>
+        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirEight.jpg" style="height: 200px"</li>
+    </ul>
+</div>
+
+
+</body>
+</html>
---
+++ wez_udzial.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dołącz do nas!</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zenek
+ * Date: 29.05.2017
+ * Time: 00:09
+ */
+
+readfile('menu.html');
+?>
+
+<div style="margin-left:10%;margin-top:10%;width: 70%">
+    <header>
+        <h1 style="font-size: 40px;">Weź udział!</h1>
+    </header>
+    <div style="float: right; width: 50%">
+    <p style="text-align: center;margin-top: 10px";>
+        <img style="position: absolute;right: 30px;width: 500px;padding: 10px" src="res/partyAfterZPI.jpg">
+    </p>
+    </div>
+    <div style="float: left; width: 50%">
+        <p style="text-align: left;font-size: 120%;">Już dzisiaj wykup wjazdówkę na festiwal Twojego życia!<br>
+            Serdecznie zapraszamy! Możesz nam dać znać, że planujesz się wybrać dołączając
+            do wydarzenia na Facebooku i polubić nasz profil FB.
+            Będziemy wdzięczni jeśli w zamian podzielisz się u siebie na FB informacją o FChO!.
+        </p>
+        <strong>Co należy zabrać ze sobą?</strong>
+        <ul>
+            <li style="text-align: left;">morze znajomych</li>
+            <li style="text-align: left;">dobry humor</li>
+            <li style="text-align: left">nuty utworu głównego - <font color="#dc143c">wykonamy go wszyscy razem!</font></li>
+        </ul>
+        <p style="font-size: 25px;">Bilety dostępne w kasach NFM oraz w sekretariacie Akademii Muzycznej</p>
+    </div>
+
+</div>
+</body>
+</html>
+
