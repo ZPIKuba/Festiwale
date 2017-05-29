26602685ed42546127769569349d8a4a0d5abe09
Zenek <zenekurwinoga@gmail.com>
2017-05-29 11:47:07 +0200

    małe poprawki

--- galeria.php
+++ galeria.php
@@ -4,6 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Galeria</title>
+    <style>
+        .galeria
+        {
+            list-style-type: none;
+            margin-top: 4%;
+
+
+        }
+        .obrazekGalerii
+        {
+            height: 350px;
+            display: block;
+            margin: auto;
+
+        }
+    </style>
 </head>
 <body>
 
@@ -20,20 +36,20 @@ readfile('menu.html');
 
 
 <div style="margin-left: 5%;width: 80%">
-    <h2 style="font-size: 25px;margin-top: 10%">
+    <h2 style="font-size: 25px;margin-top: 10%; text-align: center">
         Festiwal Chórów Osiedlowych - galeria 2015 by Stanisław Stanisławowski
     </h2>
 
     <ul style="list-style-type: none">
-        <li style="float: left;margin-left: 3%""><img src="res/galeria/choirOne.jpg" style="height: 200px"></li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirTwo.jpg" style="height: 200px" </li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirThree.jpg" style="height: 200px"</li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirNine.jpg" style="height: 200px"</li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirFour.jpg" style="height: 200px"</li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirFive.jpg" style="height: 200px"</li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirSix.jpg" style="height: 200px"</li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirSeven.jpg" style="height: 200px"</li>
-        <li style="float: left;margin-left: 3%"><img src="res/galeria/choirEight.jpg" style="height: 200px"</li>
+        <li class="galeria"><img src="res/galeria/choirOne.jpg" class="obrazekGalerii"></li>
+        <li class="galeria"><img src="res/galeria/choirTwo.jpg" class="obrazekGalerii" </li>
+        <li class="galeria"><img src="res/galeria/choirThree.jpg" class="obrazekGalerii"</li>
+        <li class="galeria"><img src="res/galeria/choirNine.jpg" class="obrazekGalerii"</li>
+        <li class="galeria"><img src="res/galeria/choirFour.jpg" class="obrazekGalerii"</li>
+        <li class="galeria"><img src="res/galeria/choirFive.jpg" class="obrazekGalerii"</li>
+        <li class="galeria"><img src="res/galeria/choirSix.jpg" class="obrazekGalerii"</li>
+        <li class="galeria"><img src="res/galeria/choirSeven.jpg" class="obrazekGalerii"</li>
+        <li class="galeria"><img src="res/galeria/choirEight.jpg" class="obrazekGalerii"</li>
     </ul>
 </div>
 
