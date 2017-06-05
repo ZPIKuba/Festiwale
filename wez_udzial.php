02f00c5ba7b0a20153d5679d3ec1b420f62e3c80
partitus <k.kozicki@windowslive.com>
2017-06-05 14:50:41 +0200

    Weź udział

--- wez_udzial.php
+++ wez_udzial.php
@@ -4,6 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Dołącz do nas!</title>
+    <style>
+        h2.info{
+            background-color: #FDD700;
+            padding: 15px 0px 0px 20px;
+            font-size: 20px;
+            font: bold 36px/45px Helvetica, Sans-Serif;
+
+        }
+        h2.info:nth-of-type(1)
+        {
+
+            background-color: #0099CC;
+        }
+        h2.info:nth-of-type(2)
+        {
+
+            background-color: #0099CC;
+        }
+        h2.info:nth-of-type(3)
+        {
+
+            background-color: #aaaaaa;
+        }
+    </style>
 </head>
 <body>
 <?php
@@ -12,33 +36,46 @@ readfile('menu.html');
 
     <div style="margin-left:10%;width: 70%;">
     <header>
-        <h1 style="font-size: 40px;">Weź udział!</h1>
+        <h2 class="info" id="wolontariat">Weź udział</h2>
     </header>
-    <div style="float: right; width: 50%">
-    <p style="text-align: center;margin-top: 10px";>
-        <img style="position: absolute;right: 30px;width: 500px;padding: 10px" src="res/partyAfterZPI.jpg">
+    <p style="text-align: center;margin-top: 10px; width: 100%; height: auto;">
+        <img style="right: 30px;width: 500px;padding: 10px" src="res/partyAfterZPI.jpg">
     </p>
+    <div style="width: 90%;text-align: justify;margin: 0% 15% auto; flex: 1;">
+        <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+    150%'><b><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>Warunki uczestnictwa:</span></b></p>
 
-    </div>
-    <div style="float: left; width: 50%">
-        <p style="text-align: left;font-size: 120%;">Już dzisiaj wykup wjazdówkę na festiwal Twojego życia!<br>
-            Serdecznie zapraszamy! Możesz nam dać znać, że planujesz się wybrać dołączając
-            do wydarzenia na Facebooku i polubić nasz profil FB.
-            Będziemy wdzięczni jeśli w zamian podzielisz się u siebie na FB informacją o FChO!.
-        </p>
-        <strong>Co należy zabrać ze sobą?</strong>
-        <ul>
-            <li style="text-align: left;">morze znajomych</li>
-            <li style="text-align: left;">dobry humor</li>
-            <li style="text-align: left">nuty utworu głównego - <font color="#dc143c">wykonamy go wszyscy razem!</font></li>
+        <ul style='margin-top:0cm' type=disc>
+            <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>W Festiwalu mogą brać udział wyłącznie chóry amatorskie,
+                    działające przy wrocławskich Radach Osiedla lub związane z osiedlami Wrocławia.</span></li>
+            <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>Czas wykonywanego programu powinien wynosić 9 - 10 minut.</span></li>
+            <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>W repertuarze powinien znaleźć się co najmniej jeden
+                    utwór zagranicznej muzyki popularnej lub ludowej (im z dalszych stron tym lepiej).</span></li>
+            <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>Prosimy o przygotowanie utworu przeznaczonego
+                    do wspólnego wykonania na zakończenie Festiwalu (nuty dołączone do zgłoszenia mailem).</span></li>
+            <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>W celu zgłoszenia uczestnictwa zespołu w Festiwalu,
+                    należy przesłać wypełnioną kartę zgłoszenia w formie elektronicznej do 24 kwietnia 2017r. na adres: ostinato@interia.pl</span></li>
+            <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>Wraz z kartą zgłoszenia do folderu festiwalowego należy
+                    przesłać następujące materiały:</span>
+                <ul style='margin-top:0cm' type=disc>
+                    <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>informację o zespole, repertuarze i osiągnięciach artystycznych (ok. 500-1000 znaków, tj. ok. 10 zdań),</span></li>
+                    <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>krótka notatka o dyrygencie (ok. 500 znaków),</span></li>
+                    <li class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+     150%'><span style='font-size:12.0pt;line-height:150%;font-family:"Times New Roman",serif'>aktualne zdjęcie chóru w formie elektronicznej.</span></li>
+                </ul></li>
         </ul>
-        <p style="font-size: 25px;">Bilety dostępne w kasach NFM oraz w sekretariacie Akademii Muzycznej</p>
+</div>
         <p style="text-align: center;margin-top: 10px">
             <img style="display: block;margin: auto;height: 600px" src="res/plakat.jpg"
         </p>
-    </div>
-
-
 </div>
 
 
